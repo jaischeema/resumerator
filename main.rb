60b70186934c965850fc8088354604e204ddc3a2
Jais Cheema <jaischeema@gmail.com>
2020-03-15 23:26:20 +1100

    Add support for SASS files for styling

--- main.rb
+++ main.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'haml'
+require 'sassc'
 
 APP_ROOT = File.dirname(__FILE__)
-$:.unshift File.join(APP_ROOT, "lib")
+$:.unshift File.join(APP_ROOT, 'lib')
 require 'resume'
 
 class Resumerator < Sinatra::Base
@@ -14,7 +17,7 @@ class Resumerator < Sinatra::Base
   end
 
   def resumes
-    @resumes ||= Resume.all_from(File.join(APP_ROOT, "resumes"))
+    @resumes ||= Resume.all_from(File.join(APP_ROOT, 'resumes'))
   end
 
   get '/' do
@@ -24,6 +27,17 @@ class Resumerator < Sinatra::Base
   get '/resumes/:index' do
     resume = resumes[params[:index].to_i - 1]
     haml resume.layout, locals: { resume: resume }
+  end
+
+  get '/css/*.css' do
+    css_file = params[:splat].first
+    file_path = File.expand_path("assets/scss/#{css_file}.scss", __dir__)
+    if File.exist?(file_path)
+      content_type 'text/css'
+      SassC::Engine.new(File.read(file_path), style: :compressed).render
+    else
+      not_found
+    end
   end
 end
 
